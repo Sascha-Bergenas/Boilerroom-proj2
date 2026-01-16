5f5671685c0b138149fde5e1e7a7da4501368ae6
Max Mattiasson <max@mattiasson.eu>
2026-01-16 14:25:11 +0100

    test

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,17 @@
+
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import Dashboard from "./pages/DashboardPage.jsx"
 
 export default function App(){
     return (
-        <Layout>
-        </Layout>
+        <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/history" element={<History />} />
+          <Route path="/third" element={<Third />} /> */}
+        </Route>
+      </Routes>
         )
 }
--- my-react-app/src/components/layout/Layout.jsx
+++ my-react-app/src/components/layout/Layout.jsx
@@ -2,15 +2,16 @@ import Header from "./Header";
 import MainContent from "./MainContent";
 import Sidebar from "./Sidebar";
 import "./layout.css";
+import { Outlet } from "react-router-dom";
 
-export default function Layout({ children }){
+export default function Layout(){
     return ( 
         <>
             <Header/>
         <div className="layout">
             <Sidebar />
-            <MainContent>{children}
-                <h2>MAAIN CONTENTO</h2>
+            <MainContent>
+                <Outlet/>
             </MainContent>
         </div>
         </>
