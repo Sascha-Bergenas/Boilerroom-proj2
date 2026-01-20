73e0405782a191138e31b8bba3701c8c97592f2b
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-01-20 15:52:31 +0100

    dashboard update on card text

--- my-react-app/src/pages/dashboard/DashboardPage.jsx
+++ my-react-app/src/pages/dashboard/DashboardPage.jsx
@@ -11,20 +11,24 @@ function Dashboard() {
         <BaseCard className={styles.timer} size="card-timer">
           <p>CARD TIMER</p>
         </BaseCard>
+
         <BaseCard className={styles.card1} size="card-large">
-          <p>CARD1</p>
+          <p>CARD 1</p>
         </BaseCard>
 
         <BaseCard className={styles.card2} size="card-medium">
           <p>CARD 2</p>
         </BaseCard>
+
         <BaseCard className={styles.card3} size="card-small">
-          <p>CARD3</p>
+          <p>CARD 3</p>
         </BaseCard>
+
         <BaseCard className={styles.card4} size="card-small">
           <h3>Logga Arbetspass</h3>
           <WorkSessionForm />
         </BaseCard>
+
         <BaseCard className={styles.card5} size="card-small">
           <h3>Kalender</h3>
         </BaseCard>
